cda057fbd0b847ef8dbec453709f903256da1cd7
amyspencerproject <amy@spencerproject.com>
2023-09-14 15:21:26 -0600

    Add Friend and FriendList component. Turn off prop-types notify in eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -30,5 +30,35 @@ export default function App() {
 }
 
 function FriendsList() {
-  return <ul>List!</ul>;
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3> {friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      {friend.balance > 0 && (
+        <p className="green">
+          Your friend {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      <button className="button"> Select </button>
+    </li>
+  );
 }
