206a76572a30ec557c18552415fc21738eee22df
amyspencerproject <amy@spencerproject.com>
2023-09-15 19:22:18 -0600

    Build the Add friend functionality

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 This app was built as an optional challenge for [The Ultimate React Course](https://www.udemy.com/course/the-ultimate-react-course/) taught by Jonas Schmedtmann
 
+## Things I learned
+
+- When I need an unique id in a new object I usually use `date()` but this time I used `crypto.randomUUID()`. This generated a very long randomw alph-numeric string. Only down side is that it will not work on older browsers. For this project it is okay but not for anything truly for the public.
+
+- Used a avatar generating api to get the friends' images. In the `image: "https://i.pravatar.cc/48?u=499476"` element the number 48 calls for an image with the size of 48 units. The ?u=499476 will assign an id to the avatar so that it can be re-loaded each time the url is called in the browser. In this app the friend's id is used for the url's id as well. When a fried is added the image element string looks like this, `image:${image}?=${id}`
+
+## Useful Resources
+
+- [crypto.randomUUID()](https://developer.mozilla.org/en-US/docs/Web/API/Crypto/randomUUID)
+- [Avatar API](https://i.pravatar.cc/48)
+
 ### Notes about Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,22 @@ const initialFriends = [
 export default function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
 
+  const [friends, setFriends] = useState(initialFriends);
+
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
   }
 
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
 
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "Close" : "Add Friend"}
@@ -51,9 +58,7 @@ function Button({ children, onClick }) {
   );
 }
 
-function FriendsList() {
-  const friends = initialFriends;
-
+function FriendsList({ friends }) {
   return (
     <ul>
       {friends.map((friend) => (
@@ -84,16 +89,50 @@ function Friend({ friend }) {
   );
 }
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48?u=");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // must have both fields to generate new friend info
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    console.log(newFriend);
+    onAddFriend(newFriend);
+
+    // reset info
+    setName("");
+    setImage("https://i.pravatar.cc/48?u=");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👫 Friend Name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🌅 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
-      <button className="button">Add</button>
+      <button type="submit" className="button">
+        Add
+      </button>
     </form>
   );
 }
